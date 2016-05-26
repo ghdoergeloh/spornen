b246511fef233b234ed36ee7e46a24de9a8b47ee
georg <georg@ghdoergeloh.de>
2016-05-26 18:01:40 +0200

    Bootstrap,JQuery und das grundlegende Layout mit Login-View

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,23 @@
+<header class="navbar navbar-default navbar-static-top" id="top"
+	role="banner">
+	<div class="container">
+		<div class="navbar-header">
+			<button class="navbar-toggle collapsed" type="button"
+				data-toggle="collapse" data-target="#bs-navbar"
+				aria-controls="bs-navbar" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span> <span
+					class="icon-bar"></span> <span class="icon-bar"></span> <span
+					class="icon-bar"></span>
+			</button>
+			<a href="../" class="navbar-brand">SponRun</a>
+		</div>
+		<nav style="height: 1px;" aria-expanded="false" id="bs-navbar"
+			class="navbar-collapse collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="#">MenuPoint</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+			</ul>
+		</nav>
+	</div>
+</header>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,45 +1,46 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
-
-        <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
-
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 96px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
-            <div class="content">
-                <div class="title">Laravel 5</div>
-            </div>
-        </div>
-    </body>
-</html>
+@extends('layouts.app') @section('title') Willkommen @endsection
+@section('content')
+<div class="row">
+	<div class="col-md-4">
+		<img class="img-responsive" alt="To All Nations" src="../images/tan_900.jpg">
+	</div>
+	<div class="col-md-4">
+		<form action="/login" method="POST">
+			<div class="form-group">
+				<h1>Anmelden</h1>
+				<label for="email">E-Mail</label> <input type="email"
+					class="form-control" id="email" placeholder="E-Mail">
+			</div>
+			<div class="form-group">
+				<label for="password">Passwort</label> <input type="password"
+					class="form-control" id="password" placeholder="Passwort">
+			</div>
+			<button type="submit" class="btn btn-default">Anmelden</button>
+		</form>
+		<form action="/register" method="POST">
+			<h1>Registrieren</h1>
+			<div class="form-group">
+				<label for="new_email">Vorname</label> <input type="text"
+					class="form-control" id="new_email" placeholder="Email">
+			</div>
+			<div class="form-group">
+				<label for="new_email">Nachname</label> <input type="text"
+					class="form-control" id="new_email" placeholder="Email">
+			</div>
+			<div class="form-group">
+				<label for="new_email">E-Mail</label> <input type="email"
+					class="form-control" id="new_email" placeholder="E-Mail">
+			</div>
+			<div class="form-group">
+				<label for="new_password">Neues Passwort</label> <input type="password"
+					class="form-control" id="new_password" placeholder="Neues Passwort">
+			</div>
+			<div class="form-group">
+				<label for="new_password_again">Passwort wiederholen</label> <input type="password"
+					class="form-control" id="new_password_again" placeholder="Passwort wiederholen">
+			</div>
+			<button type="submit" class="btn btn-default">Registrieren</button>
+		</form>
+	</div>
+</div>
+@endsection
