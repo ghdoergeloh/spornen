c885132505ead72c4005bdfda8b52a37fc801961
ghdoergeloh <georg@ghdoergeloh.de>
2018-08-31 16:40:06 +0200

    Seeder für Test-User und Sponsorenläufe

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -1,21 +1,49 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Model Factories
-|--------------------------------------------------------------------------
-|
-| Here you may define all of your model factories. Model factories give
-| you a convenient way to create models for testing and seeding your
-| database. Just tell the factory how a default model should look.
-|
-*/
+use Illuminate\Support\Str;
 
-$factory->define(App\Domain\Model\Auth\User::class, function (Faker\Generator $faker) {
-    return [
-        'firstname' => $faker->name,
-        'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
-        'remember_token' => str_random(10),
-    ];
-});
+/*
+ * |--------------------------------------------------------------------------
+ * | Model Factories
+ * |--------------------------------------------------------------------------
+ * |
+ * | Here you may define all of your model factories. Model factories give
+ * | you a convenient way to create models for testing and seeding your
+ * | database. Just tell the factory how a default model should look.
+ * |
+ */
+$factory->define(App\Domain\Model\Auth\User::class,
+	function (Faker\Generator $faker) {
+		$gender = $faker->randomElement(['m', 'f']);
+		return [
+			'firstname' => $faker->firstName($gender),
+			'lastname' => $faker->lastName,
+			'email' => $faker->unique()->safeEmail,
+			'phone' => $faker->phoneNumber,
+			'birthday' => $faker->dateTimeThisCentury,
+			'street' => $faker->streetName,
+			'housenumber' => $faker->buildingNumber,
+			'postcode' => $faker->postcode,
+			'city' => $faker->city,
+			'gender' => $gender,
+			'confirmed' => 1,
+			'password' => bcrypt(str_random(10)),
+			'remember_token' => Str::random(60)
+		];
+	});
+
+$factory->define(App\Domain\Model\Sponsor\SponsoredRun::class,
+	function (Faker\Generator $faker) {
+		$begin = $faker->dateTimeInInterval('now','+5 years');
+		return [
+			'name' => $faker->words(rand(1,6), true),
+			'begin' => $begin,
+			'end' => $faker->dateTimeInInterval($begin, '+1 day'),
+			'with_tshirt' => $faker->boolean,
+			'street' => $faker->streetName,
+			'housenumber' => $faker->buildingNumber,
+			'postcode' => $faker->postcode,
+			'city' => $faker->city,
+			'description' => $faker->text(255),
+		];
+	});
--- database/seeds/SponsoredRunsTableSeeder.php
+++ database/seeds/SponsoredRunsTableSeeder.php
@@ -13,14 +13,7 @@ class SponsoredRunsTableSeeder extends Seeder
 	 */
 	public function run()
 	{
-	    $begin = new DateTime('17.09.2016 10:00');
-	    $end = new DateTime('17.09.2016 11:00');
-	    SponsoredRun::create([
-	        'begin' => $begin,
-	        'end' => $end,
-	        'name' => 'To All Nations - Freundestag 2016',
-	        'with_tshirt' => true
-	    ]);
+		factory(SponsoredRun::class, 5)->create();
 	}
 
 }
