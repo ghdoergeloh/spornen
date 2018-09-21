548b8496e9966e9c51e08d759ff5612392daa24c
ghdoergeloh <georg@ghdoergeloh.de>
2018-09-21 12:17:34 +0200

    Anzeige des Sponsorenlaufs für den Teilnehmer breiter

--- resources/views/runparts/show.blade.php
+++ resources/views/runparts/show.blade.php
@@ -3,37 +3,35 @@
 - Teilnahme anzeigen
 @endsection
 @section('content')
-<div class="container">
-    <div class="row">
-		<div class="col-md-12">
-            <div class="card mb-3">
-                <div class="card-header">Daten</div>
-                <div class="card-body">
-					<dl class="dl-horizontal">
-						<dt>Anzahl der Sponsoren:</dt>
-						<dd>{{ $runpart->sponsors->count() }}</dd>
-					</dl>
-					<dl class="dl-horizontal">
-						<dt>Gewähltes Projekt:</dt>
-						@if (isset($runpart->project))
-						<dd>{{ $runpart->project->name }}</dd>
-						@else
-						<dd></dd>
-						@endif
-					</dl>
-					<dl class="dl-horizontal">
-						<dt>Gelaufene Runden:</dt>
-						<dd>{{ $runpart->laps }}</dd>
-					</dl>
-					<dl class="dl-horizontal">
-						<dt>Erlaufener Betrag:</dt>
-						<dd>{{ number_format($runpart->calculateDonationSum(),2,',','.') }} €</dd>
-					</dl>
-                </div>
-            </div>
-			@include('sponsors.list', ['edit' => false, 'root_route' => $root_route.'runpart.'])
-        </div>
-    </div>
+<div class="row">
+	<div class="col-md-12">
+		<div class="card mb-3">
+			<div class="card-header">Daten</div>
+			<div class="card-body">
+				<dl class="dl-horizontal">
+					<dt>Anzahl der Sponsoren:</dt>
+					<dd>{{ $runpart->sponsors->count() }}</dd>
+				</dl>
+				<dl class="dl-horizontal">
+					<dt>Gewähltes Projekt:</dt>
+					@if (isset($runpart->project))
+					<dd>{{ $runpart->project->name }}</dd>
+					@else
+					<dd></dd>
+					@endif
+				</dl>
+				<dl class="dl-horizontal">
+					<dt>Gelaufene Runden:</dt>
+					<dd>{{ $runpart->laps }}</dd>
+				</dl>
+				<dl class="dl-horizontal">
+					<dt>Erlaufener Betrag:</dt>
+					<dd>{{ number_format($runpart->calculateDonationSum(),2,',','.') }} €</dd>
+				</dl>
+			</div>
+		</div>
+		@include('sponsors.list', ['edit' => false, 'root_route' => $root_route.'runpart.'])
+	</div>
 </div>
 @include('sponsors.calculation_dlg')
 @endsection
