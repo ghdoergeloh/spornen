dd52d7cef19d767b22319d15d1d94cfc1255a84d
ghdoergeloh <georg@ghdoergeloh.de>
2018-09-21 10:53:48 +0200

    9: Newsletter für Sponsoren abfragen
    Task-Url: http://github.com/ghdoergeloh/sponrun/issues/9
    
    Migration Klassenname unique

--- database/migrations/2018_09_21_092521_add_column_wants_newsletter_sponsor.php
+++ database/migrations/2018_09_21_092521_add_column_wants_newsletter_sponsor.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddColumnWantsNewsletter extends Migration
+class AddColumnWantsNewsletterSponsor extends Migration
 {
     /**
      * Run the migrations.
