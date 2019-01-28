60aca9d7bbec87b141e519c1b19e382e85bd3c1d
ghdoergeloh <georg@ghdoergeloh.de>
2019-01-28 11:39:59 +0100

    JavaScript und Style über npm integriert

--- resources/views/layouts/footer_scripts.blade.php
+++ resources/views/layouts/footer_scripts.blade.php
@@ -1,17 +1,2 @@
 <!-- JavaScripts -->
-<!-- Bootstrap core JavaScript-->
-<script src="{{url('/theme/vendor/jquery/jquery.min.js')}}"></script>
-<script	src="{{url('/theme/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-<!-- Core plugin JavaScript-->
-<script	src="{{url('/theme/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
-<!-- Page level plugin JavaScript-->
-<script src="{{url('/theme/vendor/chart.js/Chart.min.js')}}"></script>
-<script src="{{url('/theme/vendor/datatables/jquery.dataTables.js')}}"></script>
-<script	src="{{url('/theme/vendor/datatables/dataTables.bootstrap4.js')}}"></script>
-<!-- Custom scripts for all pages-->
-<script src="{{url('/theme/js/sb-admin.min.js')}}"></script>
-<!-- Custom scripts for this page-->
-<script src="{{url('/theme/js/sb-admin-datatables.min.js')}}"></script>
-<script src="{{url('/theme/js/sb-admin-charts.min.js')}}"></script>
-<!-- Custom Script -->
-<script src="{{url('/js/custom.js')}}"></script>
+<script src="{{ asset('js/app.js') }}"></script>
--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -4,24 +4,10 @@
     <meta name="viewport"
     	content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="SponRun, eine Anwendung für die Verwaltung von Sponsorenläufen">
+    <meta name="description" content="Sponsorenläufe von {{ env('ORGANIZATION_NAME')}}">
     <meta name="author" content="Georg Dörgeloh">
     <title>{{ config('app.name') }} @yield('title')</title>
     
     <link rel="icon" type="image/x-icon" href="{{url('/favicon.ico')}}" />
-    <!-- Bootstrap core CSS-->
-    <link href="{{url('/theme/vendor/bootstrap/css/bootstrap.min.css')}}"
-    	rel="stylesheet">
-    <!-- Custom fonts for this template-->
-    <link href="{{url('/theme/vendor/font-awesome/css/font-awesome.min.css')}}"
-    	rel="stylesheet" type="text/css">
-    <!-- Page level plugin CSS-->
-    <link href="{{url('/theme/vendor/datatables/dataTables.bootstrap4.css')}}"
-    	rel="stylesheet">
-    <!-- Custom styles for this template-->
-    <link href="{{url('/theme/css/sb-admin.css')}}"
-    	rel="stylesheet">
-    <!-- Custom styles-->
-	<link href="{{url('/css/custom.css')}}"
-		rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
