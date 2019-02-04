0a1c871a40faa7e96229e6c54547d37e91b418df
ghdoergeloh <georg@ghdoergeloh.de>
2019-02-04 17:35:37 +0100

    copyToClipboard repariert

--- resources/assets/js/custom.js
+++ resources/assets/js/custom.js
@@ -9,53 +9,62 @@ $(document).ready(function() {
 		});
 	}
 	
-	$(function() {
-		$('form.ajax-submit').submit(function(event) {
-			event.preventDefault();
-			var form = this;
-			var formData = $(form).serialize();
-			$.ajax({
-				type : $(form).attr('method'),
-				url : $(form).attr('action'),
-				dataType : 'JSON',
-				data : formData
-			})
-			.done(function(data) {
-				$(form).trigger('afterSubmit');
-			})
-			.fail(function(data){
-				console.log(data);
-			});
-		});
+	$('#share-link-button').click(function(event){
+		let element = $('#share-link');
+		element.select();
+		document.execCommand("copy");
+		$(element).attr('data-title', "Kopiert").tooltip('show');
 	});
 
+	$('#share-link-button').mouseleave(function(event){
+		$('#share-link').tooltip('dispose');
+	});
+	
+	$('form.ajax-submit').submit(function(event) {
+		event.preventDefault();
+		let form = this;
+		let formData = $(form).serialize();
+		$.ajax({
+			type : $(form).attr('method'),
+			url : $(form).attr('action'),
+			dataType : 'JSON',
+			data : formData
+		})
+		.done(function(data) {
+			$(form).trigger('afterSubmit');
+		})
+		.fail(function(data){
+			console.log(data);
+		});
+	});
+	
 	$('.editableLaps').click(function(){
 		if($(this).children().length == 0){
-			var cell = this;
-			var row = $(cell).parent();
-			var initialLaps = $(cell).text();
+			let cell = this;
+			let row = $(cell).parent();
+			let initialLaps = $(cell).text();
 			$(cell).text('');
 
-			var div1 = document.createElement('div');
+			let div1 = document.createElement('div');
 			$(cell).append(div1);
 			$(div1).attr('class','input-group');
 			
-			var input = document.createElement('input');
+			let input = document.createElement('input');
 			$(div1).append(input);
 			$(input).attr('class','form-control');
 			$(input).attr('type','number');
 			$(input).attr('value', initialLaps);
 			
 
-			var div2 = document.createElement('div');
+			let div2 = document.createElement('div');
 			$(div1).append(div2);
 			$(div2).attr('class','input-group-append');
 			
-			var button = document.createElement('button');
+			let button = document.createElement('button');
 			$(div2).append(button);
 			$(button).attr('class','btn btn-outline-secondary');
 			
-			var icon = document.createElement('i');
+			let icon = document.createElement('i');
 			$(button).append(icon);
 			$(icon).attr('class','fa fa-save');
 
@@ -64,11 +73,11 @@ $(document).ready(function() {
 			$(input).focus();
 			$(input).select();
 			$(input).bind("submit",function(e){
-				var runPartId = $(row).attr('row_id');
-				var laps = $(input).val();
-				var url = indexRunPartURL + "/" + runPartId;
-				var CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
-				var result = $.ajax({
+				let runPartId = $(row).attr('row_id');
+				let laps = $(input).val();
+				let url = indexRunPartURL + "/" + runPartId;
+				let CSRF_TOKEN = $('meta[name="csrf-token"]').attr('content');
+				let result = $.ajax({
 					url : url,
 					type : 'PATCH',
 					dataType : 'JSON',
@@ -116,29 +125,3 @@ $(document).ready(function() {
 
 	$('.dataTable').DataTable();
 });
-
-
-function selectText(element) {
-	var range, selection;
-	if (document.body.createTextRange) {
-		range = document.body.createTextRange();
-		range.moveToElementText(element);
-		range.select();
-	} else if (window.getSelection) {
-		selection = window.getSelection();
-		range = document.createRange();
-		range.selectNodeContents(element);
-		selection.removeAllRanges();
-		selection.addRange(range);
-	}
-}
-
-function copyToClipboard(element) {
-	element.select();
-	document.execCommand("copy");
-	$(element).attr('data-title', "Kopiert").tooltip('show');
-}
-
-function disposeTooltip(element, text) {
-	$(element).tooltip('dispose');
-}
--- resources/views/formfields/share_link.blade.php
+++ resources/views/formfields/share_link.blade.php
@@ -3,12 +3,11 @@
 
 	<div class="input-group">
 		{{ Form::output('share_link', null, [
+			'id' => 'share-link',
 			'class' => 'form-control'
 		]) }}
 		<div class="input-group-append">
-			<button class="btn btn-outline-secondary"
-				type="button" onclick="copyToClipboard($(this).parent().prev());"
-				onmouseout="disposeTooltip($(this).parent().prev(), 'Link kopieren');">
+			<button id="share-link-button" class="btn btn-outline-secondary" type="button">
 				<span class='fa fa-share-alt form-control-feedback'
 					aria-hidden='true'></span> Kopieren
 			</button>
