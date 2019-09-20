b549becbb091d932bd188a4db96026f26284370e
ghdoergeloh <georg@ghdoergeloh.de>
2019-09-20 22:07:21 +0200

    npm update with fixes

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,10 @@
 require('./bootstrap');
 require('jquery.easing');
-require('fittextjs');
 
 require('startbootstrap-sb-admin/vendor/chart.js/Chart.min.js');
-require('startbootstrap-sb-admin/vendor/datatables/dataTables.bootstrap4.js');
 require('startbootstrap-sb-admin/vendor/datatables/jquery.dataTables.js');
+require('startbootstrap-sb-admin/vendor/datatables/dataTables.bootstrap4.js');
 require('startbootstrap-sb-admin/js/sb-admin.min.js');
 
+require('fittextjs');
 require('./custom');
